a60d575eab4c9a97605b8c6a90efa999d5c8167c
rajurao <rajurao1107@gmail.com>
2025-02-17 20:49:30 +0530

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/taskmanager", {
+    await mongoose.connect("mongodb://localhost/taskmanager", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
