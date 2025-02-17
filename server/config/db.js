6cacce3407c15197d177031e5fedf65a99ea5478
rajurao <rajurao1107@gmail.com>
2025-02-17 22:13:44 +0530

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/taskmanager");
+    await mongoose.connect("mongodb+srv://rajurao1107:raoraju1337@cluster0.zjucb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error.message);
